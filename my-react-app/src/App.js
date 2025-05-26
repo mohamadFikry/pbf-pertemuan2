0e60f2fd836a32761f137729aaa04cfc3b6a4c51
fikri <fibecvii@gmail.com>
2025-05-26 12:14:46 +0700

    update praktikum

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Counter from './Counter';
+import Greeting from './Greeting';
+
+
+function Header() {
+  return (
+    <header>
+      <h1>Aplikasi React Saya</h1>
+    </header>
+  )
+}
+
+function Main() {
+  return (
+  <main>
+    <h2>Selamat datang di Aplikasi React Saya</h2>
+    <p>Ini adalah konten utama.</p>
+  </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2025 Aplikasi React Saya</p>
+    </footer>
+  )
+}
+
+function Example() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type='text' placeholder='Nama' value={name} onChange={handleNameChange}/>
+      <input type='number' placeholder='Umur' value={age} onChange={handleAgeChange}/>
+      <input type='email' placeholder='Email' value={email} onChange={handleEmailChange}/>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Main />
+      <Greeting name="John" />
+      <Counter />
+      <Example />
+      <Footer />
     </div>
   );
 }
